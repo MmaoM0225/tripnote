f687893cb57b816769f2de077d48b2e414fabe26
Mao <1920442561@qq.com>
2025-05-06 09:19:48 +0800

    用户端： 详情页面静态实现

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -2,48 +2,64 @@ import { View, Text, Image, Video, Input, Swiper, SwiperItem } from '@tarojs/com
 import { AtIcon, AtButton } from 'taro-ui'
 import './index.scss'
 
+const videoUrl = 'https://www.w3schools.com/html/mov_bbb.mp4' // 可改为动态获取
+const imageUrls = [1, 2, 3].map(i => `https://picsum.photos/600/400?random=${i}`)
+
 export default function DetailPage() {
   return (
     <View className='detail-page'>
 
       {/* 吸顶栏 */}
       <View className='sticky-header'>
-        <AtIcon value='chevron-left' size='24' />
+
         <View className='author-info'>
+          <AtIcon value='chevron-left' size='24' />
           <Image className='avatar' src='https://randomuser.me/api/portraits/men/11.jpg' />
           <Text className='name'>作者名</Text>
         </View>
+        <View>
         <AtButton size='small' type='primary'>关注</AtButton>
+        </View>
       </View>
 
       {/* 主体内容 */}
       <View className='content'>
-
-        <Text className='title'>一次难忘的云南之旅</Text>
-
         <View className='info-box'>
-          <Text>地点：丽江</Text>
-          <Text>时间：2025年5月</Text>
-          <Text>花费：¥5000</Text>
-          <Text>季节：春季</Text>
+          <View className='info'>
+            <Text>地点</Text>
+            <Text className='detail'>丽江</Text>
+          </View>
+          <View className='info'>
+            <Text>时间</Text>
+            <Text className='detail'>2025年5月</Text>
+          </View>
+          <View className='info'>
+            <Text>花费</Text>
+            <Text className='detail'>¥5000</Text>
+          </View>
+          <View className='info'>
+            <Text>行程天数</Text>
+            <Text className='detail'>8天</Text>
+          </View>
         </View>
 
-        <Swiper className='image-swiper' circular autoplay>
-          {[1, 2, 3].map(i => (
+        <Swiper className='image-swiper' indicatorDots  >
+          {videoUrl && (
+            <SwiperItem>
+              <Video
+                src={videoUrl}
+                className='swiper-video'
+                controls
+              />
+            </SwiperItem>
+          )}
+          {imageUrls.map((url, i) => (
             <SwiperItem key={i}>
-              <Image className='swiper-img' src={`https://picsum.photos/600/400?random=${i}`} mode='aspectFill' />
+              <Image className='swiper-img' src={url} mode='aspectFill' />
             </SwiperItem>
           ))}
         </Swiper>
-
-        <View className='video-wrapper'>
-          <Video
-            src='https://www.w3schools.com/html/mov_bbb.mp4'
-            className='video'
-            controls
-          />
-        </View>
-
+        <Text className='title'>一次难忘的云南之旅</Text>
         <View className='article-content'>
           <Text>
             这次旅行收获颇丰，蓝天、白云、雪山与古镇构成了梦幻的旅程……
